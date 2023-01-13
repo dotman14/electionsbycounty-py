2b4924bdf96a7ac8d1b4548b0c8c6795224134c0
Oyedotun Oyesanmi <dotunoyesanmi@gmail.com>
2023-01-13 10:17:59 -0600

    testing out cache conteol header

---
+++ electionsbycounty/middleware/public_cache.py
@@ -0,0 +1,8 @@
+class PublicCacheControlMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response['Cache-Control'] = 'public, max_age=5184000'
+        return response
--- electionsbycounty/settings.py
+++ electionsbycounty/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "electionsbycounty.middleware.public_cache.PublicCacheControlMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
